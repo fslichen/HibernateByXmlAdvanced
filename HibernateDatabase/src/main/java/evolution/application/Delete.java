05f12feda78ca58dbf8fcb81c46b6b49975be9ea
user <user@use1r-PC>
2017-02-15 15:00:43 +0800

    new demo

---
+++ HibernateDatabase/src/main/java/evolution/application/Delete.java
@@ -0,0 +1,17 @@
+package evolution.application;
+
+import org.hibernate.Session;
+
+import evolution.AnySession;
+import evolution.entity.Student;
+
+public class Delete {
+	public static void main(String[] args) {
+		Session session = AnySession.getSession();
+		Student student = new Student();
+		student.setId(3);
+		session.delete(student);
+		session.getTransaction().commit();
+		AnySession.close();// Stop the program.
+	}
+}
